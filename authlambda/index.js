66714d425b61c8987a1633734f558c89a18615bb
Dylan <dylanblee@gmail.com>
2024-04-17 16:11:09 -0400

    edited authlambda

--- authlambda/index.js
+++ authlambda/index.js
@@ -4,17 +4,22 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 const dynamoDBClient = new DynamoDBClient({  region: "us-east-1" });
 const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
-const { QueryCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { QueryCommand, PutCommand, DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 const crypto = require('crypto');
 const ses = new SESClient({ region: "us-east-1"});
 const tableName = "TokenStorage";
-const whitelistDomains = ['ua.edu', 'noaa.gov']; // Add your domains here
-const whitelistEmails = ['dylanblee@gmail.com']; // Add specific emails here
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async (event) => {
+    //load whitelist
+    const whitelist = await loadWhitelist();
+    const whitelistDomains = whitelist.domains;
+    const whitelistEmails = whitelist.emails;
+
     try {
         const { email } = JSON.parse(event.body);
-        if (!email || !validateEmail(email)) {
+        if (!email || !validateEmail(email, whitelistDomains, whitelistEmails)) {
             return sendResponse(400, { message: 'Invalid email format or domain not allowed.' });
         }
 
@@ -25,13 +30,17 @@ exports.handler = async (event) => {
         }
 
         const token = generateToken();
-        const currentDate = new Date().toISOString();
-	    //
-	// Store in DynamoDB
-	await docClient.send(new PutCommand({
-	    TableName: tableName,
-	    Item: { email, token, generationDate: currentDate }
-	}));
+        const currentDate = new Date();
+        const expiryDate = new Date(currentDate.getTime() + (91 * 24 * 60 * 60 * 1000)); // 91 days from now
+
+        // Convert expiryDate to Unix epoch time in seconds
+        const timeToExist = Math.floor(expiryDate.getTime() / 1000);
+	    
+	    // Store in DynamoDB
+	    await docClient.send(new PutCommand({
+	        TableName: tableName,
+	        Item: { email, token, generationDate: currentDate.toISOString(), TimeToExist: timeToExist }
+	    }));
         // Send email
         const emailSent = await sendEmail(email, token);
 
@@ -42,12 +51,31 @@ exports.handler = async (event) => {
     }
 };
 
+async function loadWhitelist() {
+    const whitelistPath = path.join(__dirname, 'whitelist'); // Adjust if your file is in a subdirectory
+    return new Promise((resolve, reject) => {
+        fs.readFile(whitelistPath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading whitelist file:', err);
+                return reject(err);
+            }
+            try {
+                const whitelist = JSON.parse(data);
+                return resolve(whitelist);
+            } catch (parseError) {
+                console.error('Error parsing whitelist file:', parseError);
+                return reject(parseError);
+            }
+        });
+    });
+}
+
 async function countRecentTokens(email) {
     const ninetyDaysAgo = new Date(Date.now() - (90 * 24 * 60 * 60 * 1000)).toISOString();
     // Count recent tokens
     const result = await docClient.send(new QueryCommand({
 	TableName: tableName,
-	IndexName: 'EmailIndex', // Make sure to create this index on the 'email' attribute in your DynamoDB table
+	IndexName: 'EmailIndex', 
 	KeyConditionExpression: 'email = :email and generationDate >= :ninetyDaysAgo',
 	ExpressionAttributeValues: {
 	    ':email': email,
@@ -57,7 +85,8 @@ async function countRecentTokens(email) {
     return result.Items.length;
 }
 
-function validateEmail(email) {
+
+function validateEmail(email, whitelistDomains, whitelistEmails) {
     const domain = email.split('@')[1];
     return whitelistDomains.includes(domain) || whitelistEmails.includes(email);
 }
@@ -70,13 +99,15 @@ async function sendEmail(recipientEmail, token) {
     // Send email
     try {
 	await ses.send(new SendEmailCommand({
-	    Source: 'dylan@waternode.ciroh.org', // Replace with your "from" address
+	    Source: 'dylan@waternode.ciroh.org',
 	    Destination: { ToAddresses: [recipientEmail] },
 	    Message: {
-		Subject: { Data: 'Your Access Token' },
+		Subject: { Data: 'Your Water Prediction Node Access Token' },
 		Body: {
-		    Text: { Data: `Your token is: ${token}` }
-		}
+		  Text: {
+		    Data: `Greetings from the Water Prediction Node!\n\nYour token is: ${token}\n\nThis token is good for 90 days and can be used to access the Water Prediction Node experimental data catalog using python and the pystac library or the experimental catalog browser at https://waternode.ciroh.org/shhcat` 
+		  }
+		  }
 	    }
 	}));
 	return true;
